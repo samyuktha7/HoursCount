6e73c3e1d3d6446adfb91c078847a6248b3b7187
smadired <Samyuktha.Madireddi@windriver.com>
2019-04-08 16:32:26 -0400

    * Sheets fragment now has recylcer view, which displays a list of weekly stored hours from the database. * Performed Cleaning operations on Database and related files. deleterd unnecessary files. * Modified changes in XML files

--- app/src/main/java/countedhours/hourscount/Database/SqLiteDatabaseHelper.java
+++ app/src/main/java/countedhours/hourscount/Database/SqLiteDatabaseHelper.java
@@ -7,17 +7,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import com.google.android.gms.location.Geofence;
-
 
 /*
 This class is SqLIteOpenHelper which performs all the operations such as reading and writing to databases
 Database name = timeSheets.db
-Table names :
-    1.ADDRESS_INFO
-    2.CHECK_TIMINGS_TABLE
-    3.WEEK_TIMINGS
-    4.SHEETS_TABLE
+Table name :  SHEETS_TABLE
  */
 public class SqLiteDatabaseHelper extends SQLiteOpenHelper {
 
@@ -26,11 +20,6 @@ public class SqLiteDatabaseHelper extends SQLiteOpenHelper {
     public SQLiteDatabase mDatabase;
     private static SqLiteDatabaseHelper mInstance = null;
     private String TAG = "SqLiteDatabaseHelper";
-
-    public SqLiteDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
-    }
-    private Context mCxt;
 
     public static SqLiteDatabaseHelper getInstance(Context ctx) {
         Log.d("SqLiteDatabaseHelper", "SqLiteDatabaseHelper(): getInstance()");
@@ -53,7 +42,6 @@ public class SqLiteDatabaseHelper extends SQLiteOpenHelper {
     private SqLiteDatabaseHelper(Context ctx) {
         super(ctx, DATABASE_NAME, null, DATABASE_VERSION);
         Log.d(TAG, "SqLiteDatabaseHelper() constructor");
-        mCxt= ctx;
     }
 
     
@@ -61,68 +49,11 @@ public class SqLiteDatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         Log.d(TAG, "onCreate()");
         sqLiteDatabase.execSQL(SheetsData.CREATE_TABLE_SHEETS_DATA);
-        sqLiteDatabase.execSQL(CheckInOutTimings.CREATE_TABLE_CHECKINOUT_TIMINGS);
-        sqLiteDatabase.execSQL(AddressInformation.CREATE_TABLE_ADDRESS_INFORMATION);
-        sqLiteDatabase.execSQL(WeekData.CREATE_TABLE_WEEK_TIMINGS);
-        sqLiteDatabase.execSQL(GeofenceTransitions.CREATE_TABLE_GEOFENCE_TRANSITIONS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
-    }
-
-    public synchronized SQLiteDatabase openDatabase() {
-        mDatabase = this.getWritableDatabase();
-        return mDatabase;
-    }
-
-    /*
-    Resets ths values in GeofenceTransitionTable. Usually cleared once every day.
-     */
-    public synchronized void resetGeofencevalues() {
-        Log.d(TAG, "resetGeofenceValues()");
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.execSQL("delete from "+ GeofenceTransitions.GEO_FENCE_TRANSITIONS_TABLE);
-        db.close();
-        retrieveGeofenceValues();
-    }
-
-    public GeofenceTransitions getLastSavedGeofenceTransition() {
-        Log.d(TAG, "getLastSavedGeofenceTransition()");
-        Cursor dbCursor = retrieveGeofenceValues();
-        GeofenceTransitions geofenceTransition = new GeofenceTransitions();
-        if(dbCursor.getCount() >0) {
-            dbCursor.moveToPosition(dbCursor.getCount() - 1);
-            geofenceTransition.setTransitionType(dbCursor.getInt(dbCursor.getColumnIndex(GeofenceTransitions.TRANSITION_TYPE)));
-            geofenceTransition.setTimeOccurred(dbCursor.getLong(dbCursor.getColumnIndex(GeofenceTransitions.TIME_OCCURRED)));
-        } else {
-            Log.d(TAG, "getLastSavedAddressInfo(): database is empty");
-        }
-        return getLastSavedGeofenceTransition();
-    }
-
-    /*
-    Fetches the values in GEO_FENCE_TRANSITIONS_TABLE TABLE.
-     */
-    public Cursor retrieveGeofenceValues() {
-        Log.d(TAG, "retrieveGeofenceValues()");
-        String selectQuery = "SELECT  * FROM " + GeofenceTransitions.GEO_FENCE_TRANSITIONS_TABLE;
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery(selectQuery, null);
-        Log.d(TAG, "retreiveGeofenceValues(): cursor count :"+cursor.getCount());
-        return cursor;
-    }
-
-    public int insertCheckInOutValues(long check_in, long check_out) {
-        Log.d(TAG, "insertCheckInOutValues()");
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(CheckInOutTimings.CHECK_IN, check_in);
-        values.put(CheckInOutTimings.CHECK_OUT, check_out);
-        int row_id=(int)db.insert(CheckInOutTimings.CHECK_TIMINGS_TABLE, null, values);
-        db.close();
-        return row_id;
     }
 
     public int insertSheetsValues(String weekEnd, Float hours) {
@@ -136,70 +67,12 @@ public class SqLiteDatabaseHelper extends SQLiteOpenHelper {
         return row_id;
     }
 
-    public int insertAddressInfoValues(String Address, Double latitude, Double longitude) {
-        Log.d(TAG, "insertAddressInfoValues()");
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(AddressInformation.ADDRESS, Address);
-        values.put(AddressInformation.LATITUDE, latitude);
-        values.put(AddressInformation.LONGITUDE, longitude);
-        int row_id=(int)db.insert(AddressInformation.ADDRESS_INFO, null, values);
-        db.close();
-        return row_id;
-    }
-
-    public int insertWeekValues(String weekday, float totalHours) {
-        Log.d(TAG, "insertWeekValues()");
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(WeekData.DATE, weekday);
-        values.put(WeekData.HOURS, totalHours);
-        int row_id=(int)db.insert(WeekData.WEEK_TIMINGS, null, values);
-        db.close();
-        return row_id;
-    }
-
-    public AddressInformation fetchAddressGeo(String address) {
-        Log.d(TAG, "fetch Address Geo Locations from Database");
-        AddressInformation addressInformation = new AddressInformation();
-        if(address != null) {
-            Cursor dbCursor = retrieveAddressInfo();
-            while(dbCursor.moveToNext()) {
-                if (dbCursor.getString(dbCursor.getColumnIndex(AddressInformation.ADDRESS)).equals(address)){
-                    addressInformation.setAddress(dbCursor.getString(dbCursor.getColumnIndex(AddressInformation.ADDRESS)));
-                    addressInformation.setLatitude(dbCursor.getDouble(dbCursor.getColumnIndex(AddressInformation.LATITUDE)));
-                    addressInformation.setLongitude(dbCursor.getDouble(dbCursor.getColumnIndex(AddressInformation.LONGITUDE)));
-                    return addressInformation;
-                }
-            }
-            Log.d(TAG,"fetchGeoAddress(): address not found in database");
-        } else {
-            Log.w(TAG, "fetchGeoAddress(): address is null");
-        }
-        return null;
-    }
-
-    public AddressInformation getLastSavedAddressInfo() {
-        Log.d(TAG, "getLastSavedAddressInfo()");
-        Cursor dbCursor = retrieveAddressInfo();
-        AddressInformation lastSavedAddress= new AddressInformation();
-        if(dbCursor.getCount() >0) {
-            dbCursor.moveToPosition(dbCursor.getCount() - 1);
-            lastSavedAddress.setAddress(dbCursor.getString(dbCursor.getColumnIndex(AddressInformation.ADDRESS)));
-            lastSavedAddress.setLatitude(dbCursor.getDouble(dbCursor.getColumnIndex(AddressInformation.LATITUDE)));
-            lastSavedAddress.setLongitude(dbCursor.getDouble(dbCursor.getColumnIndex(AddressInformation.LONGITUDE)));
-        } else {
-            Log.d(TAG, "getLastSavedAddressInfo(): database is empty");
-        }
-        return lastSavedAddress;
-    }
-
-    public Cursor retrieveAddressInfo() {
-        Log.d(TAG, "retrieveAddressInfo()");
-        String selectQuery = "SELECT  * FROM " + AddressInformation.ADDRESS_INFO;
+    public Cursor retrieveSheetsInfo() {
+        Log.d(TAG, "retrieveSheetsInfo()");
+        String selectQuery = "SELECT  * FROM " + SheetsData.SHEETS_TABLE;
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery(selectQuery, null);
-        Log.d(TAG, "retreiveAddressInfo(): cursor count :"+cursor.getCount());
+        Log.d(TAG, "retreiveSheets(): cursor count :"+cursor.getCount());
         return cursor;
     }
 }
--- app/src/main/java/countedhours/hourscount/Fragments/Sheets_Fragment.java
+++ app/src/main/java/countedhours/hourscount/Fragments/Sheets_Fragment.java
@@ -1,24 +1,115 @@
 package countedhours.hourscount.Fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import countedhours.hourscount.Database.SheetsData;
+import countedhours.hourscount.Database.SqLiteDatabaseHelper;
 import countedhours.hourscount.R;
 
 
 public class Sheets_Fragment extends Fragment {
 
+    private String TAG = "HC_"+Sheets_Fragment.class.getSimpleName();
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private SqLiteDatabaseHelper dbhelper;
+    private Cursor mCursor;
+    public List<SheetsData> data;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sheets, container, false);
+        View v = inflater.inflate(R.layout.fragment_sheets, container, false);
+        Log.d(TAG, "onCreateView");
+
+        dbhelper = SqLiteDatabaseHelper.getInstance(this.getActivity());
+
+        /*
+        Retrieves Information from Sheets database - which stores the week end date and number of
+        hours worked during that week. Fetches the cursor and changes into list, which is updated
+        to the adapter of recylcer view.
+         */
+        mCursor = dbhelper.retrieveSheetsInfo();
+        if (mCursor != null) {
+            data = new ArrayList<>();
+            while (mCursor.moveToNext()) {
+                SheetsData sheet = new SheetsData();
+                sheet.setWeekEnd(mCursor.getString(0));
+                sheet.setHours(mCursor.getFloat(1));
+                data.add(sheet);
+                Log.d(TAG, "sheet added");
+            }
+
+            recyclerView = (RecyclerView) v.findViewById(R.id.my_recycler_view);
+            // use a linear layout manager
+            layoutManager = new LinearLayoutManager(this.getActivity());
+            recyclerView.setLayoutManager(layoutManager);
+
+            MyAdapter adapter = new MyAdapter();
+            recyclerView.setAdapter(adapter);
+
+        } else {
+            Log.w(TAG, "Sheets Cursor is null");
+        }
+        return v;
     }
 
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyviewHolder> {
+
+        @NonNull
+        @Override
+        public MyAdapter.MyviewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            Log.d(TAG, "onCreateViewHolder");
+            View v= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.eachlayout, viewGroup, false);
+            return new MyviewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyAdapter.MyviewHolder myviewHolder, int i) {
+            Log.d(TAG, "onBindViewHolder");
+            if (data != null) {
+                SheetsData sheetData = data.get(i);
+                myviewHolder.weekNumber.setText(sheetData.getWeekEnd());
+                myviewHolder.noOfHours.setText(String.valueOf(sheetData.getHours()));
+            } else {
+                Log.d(TAG, "onBindViewHolder() : sheets data is null");
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            int count = data.size();
+            Log.d(TAG, "getItemCount() = " +count);
+            return count;
+        }
+
+        public class MyviewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            private TextView weekNumber, noOfHours;
+            private MyviewHolder(View itemView) {
+                super(itemView);
+                weekNumber = itemView.findViewById(R.id.weekName);
+                noOfHours = itemView.findViewById(R.id.noOfHours);
+            }
+        }
+
+    }
 
 }
